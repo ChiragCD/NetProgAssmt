5a9894e7ff073a180d948d9b7b45ef5a479a9958
ChiragCD <chirag8cd@gmail.com>
2020-09-30 20:02:17 +0530

    Stuff

--- shell.c
+++ shell.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+void shell(int argc, char ** argv) {
+    while(1) {
+        accept();
+        execute();
+    }
+    return;
+}
 
 int main (int argc, char ** argv) {
+    shell(argc, argv);
     return 0;
 }
