b92929ce9e4fcc96b292e0d7e58f9c9477ab4f62
ChiragCD <chirag8cd@gmail.com>
2020-10-04 17:53:36 +0530

    I'm stuck

--- client.c
+++ client.c
@@ -20,14 +20,16 @@ void client() {
     msg send_buf,recv_buf;
     ssize_t temp, msgsize;
     for(;;){
-            printf("Enter the type of operation you would like to perform:\n1. ADD FILE <file path>\n2. ADD CHUNK <file path> <machine file path> <chunk number>\n3. COPY <source> <destination>\n4. MOVE <source> <destination>\n5. REMOVE <file path>\n6. COMMAND <command> <chunk name>\n");
-            int choice;
-            choice = getchar();
+            printf("\nEnter the type of operation you would like to perform:\nADD FILE: 1 <file path>\nADD CHUNK: 2 <file path> <machine file path> <chunk number>\nCOPY: 3 <source> <destination>\nMOVE: 4 <source> <destination>\nREMOVE: 5 <file path>\nCOMMAND: 6 <command> <chunk name>\n");
+            int choice = -1;
+            choice = getc(stdin);
+            printf("%d\n", choice);
             choice -= '0';
             char cmd[100],s[100],d[100],fp[100];
             int chunk_num;
             switch(choice){
                 case 1:scanf("%s",s); // take in input the desired location in M.
+                getchar();
                        send_buf.mtype=1;
                        send_buf.mbody.req = ADD_FILE;
                        send_buf.mbody.sender = getpid();
@@ -42,6 +44,7 @@ void client() {
                 case 2:scanf("%s",s); 
                        scanf("%s",fp); // get the local file path inside client file-directory
                        scanf("%d",&chunk_num);
+                getchar();
                        send_buf.mtype=1;
                        send_buf.mbody.req = ADD_CHUNK;
                        send_buf.mbody.sender = getpid();
@@ -52,14 +55,18 @@ void client() {
                                break;
                        }
                        chunk c;
-                       lseek(fd,MSGSIZE*7/8*chunk_num,SEEK_SET);
+                       lseek(fd,MSGSIZE/2*chunk_num,SEEK_SET);
                        if(read(fd,c.data,MSGSIZE/2) == 0 )
                        {printf("Chunk number too large, file is not that big\n");close(fd);break;}
+                       close(fd);
+                       printf("Data %s\n", c.data);
 
                        temp = msgsnd(mqid,&send_buf,MSGSIZE,0);
                        msgsize = msgrcv(mqid, &recv_buf, MSGSIZE, getpid(), 0);
+                       printf("received\n");
                        if(recv_buf.mbody.status==-1)
-                       {printf("%s\n",recv_buf.mbody.error);close(fd);break;}
+                       {printf("%d %s\n",recv_buf.mbody.status, recv_buf.mbody.error);close(fd);break;}
+                       printf("Entering loop\n");
                        for(int i=0;i<3;i++){
                                pid_t d_pid = recv_buf.mbody.addresses[i];
                                send_buf.mtype=d_pid;
@@ -68,9 +75,11 @@ void client() {
                                send_buf.mbody.req=STORE_CHUNK;
                                ssize_t temp = msgsnd(mqid,&send_buf,MSGSIZE,0);
                        }
+                       printf("Done\n");
                        break;
                 case 3:scanf("%s",s);
                        scanf("%s",d);
+                getchar();
                        send_buf.mtype=1;
                        send_buf.mbody.req = CP;
                        send_buf.mbody.sender = getpid();
@@ -85,6 +94,7 @@ void client() {
                        break;
                 case 4:scanf("%s",s);
                        scanf("%s",d);
+                getchar();
                        send_buf.mtype=1;
                        send_buf.mbody.req = MV;
                        send_buf.mbody.sender = getpid();
@@ -98,6 +108,7 @@ void client() {
                                printf("FILE MOVED SUCCESSFULLY\n");
                        break;
                 case 5:scanf("%s",s); // take in input the desired location in M.
+                getchar();
                        send_buf.mtype=1;
                        send_buf.mbody.req = RM;
                        send_buf.mbody.sender = getpid();
@@ -111,6 +122,7 @@ void client() {
                        break;
                 case 6:scanf("%s",cmd);
                        scanf("%s",s);
+                getchar();
                        send_buf.mtype=1;
                        send_buf.mbody.req = COMMAND;
                        send_buf.mbody.sender = getpid();
@@ -122,8 +134,11 @@ void client() {
                        else 
                                printf("FILE ADDED SUCCESSFULLY\n");
                        break;
-                        
-
+                case -49:
+                printf("njwefk\n");
+                    int tempkroqnnw = getc(stdin);
+                    printf("%d\n", tempkroqnnw);
+                break;
 
             }
     }
